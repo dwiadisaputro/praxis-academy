d64fa3c8134945457bc2586b736df106a15727f5
dwiadisaputro <asmbrt@gmail.com>
2022-04-12 16:29:02 +0700

    tambah data

--- akademik/01-01/kasus/coba.py
+++ akademik/01-01/kasus/coba.py
@@ -88,5 +88,37 @@
 # print(squares[:])
 # print(squares + [36, 49, 64, 81, 100])
 
-cubes = [1, 8, 27, 64, 81, 100]
-print(4 ** 3)
+cubes = [1, 8, 27, 64, 81, 125]
+# print(4 ** 3)
+# cubes[3] = 64
+# print(cubes)
+# cubes.append(216)
+# cubes.append(7 ** 3)
+# print(cubes)
+
+# letters = ["a", "b", "c", "d", "e", "f", "g"]
+# # print(letters)
+# letters [2:5] = ["C", "D", "E"]
+# # print (letters)
+# letters[:] = []
+# print(letters)
+
+# a = ["a", "b", "c"]
+# n = [1, 2, 3]
+# x = [a, n]
+# print(x)
+# print(x[0])
+# print(x[0][1])
+
+# a, b = 0, 1
+# while a < 10:
+#   print(a)
+#   a, b = b, a+b
+
+i = 256*256
+  print ( "The value of i is", i )
+
+# a, b = 0, 1
+# while a < 1000:
+#   print(a, end=",")
+#   a, b = b, a+b
