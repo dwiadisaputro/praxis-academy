59821c15508841f4f56583bbc4f76c24f4380177
dwiadisaputro <asmbrt@gmail.com>
2022-04-21 16:25:18 +0700

    Update tugas minggu ke 2 tentang Pengujian Python

--- NOVICE/minggu02/hari02/project/src/test.py
+++ NOVICE/minggu02/hari02/project/src/test.py
@@ -17,7 +17,7 @@
 
 from fractions import Fraction
 import unittest
-# from my_sum import sum
+from my_sum import sum
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
@@ -26,7 +26,7 @@ class TestSum(unittest.TestCase):
  """
         data = [1, 2, 3]
         result = sum(data)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, 6)
 
  def test_list_fraction(self):  """  Test that it can sum a list of fractions  """  data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]  result = sum(data)  self.assertEqual(result, 6) 
 if __name__ == '__main__':
