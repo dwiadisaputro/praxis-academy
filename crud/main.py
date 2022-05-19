829643edfc9bc4eaeb0cf577c6394ad8b65ac657
dwiadisaputro <asmbrt@gmail.com>
2022-05-19 15:23:37 +0700

    kasus koneksi ke atlas

--- crud/main.py
+++ crud/main.py
@@ -1,19 +1,11 @@
-from dotenv import load_env, find_env
+from dotenv import find_dotenv, load_dotenv
 from pymongo import MongoClient
 import os
 import pprint
-load_env(find_env())
+
+load_dotenv(find_dotenv())
 
 password = os.environ.get("MONGODB_PWD")
+print(password)
 connection_string = f"mongodb+srv://dwi:{password}@cluster0.ylbba.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
-
-
-
-
-
-
-
-
-# if __name__=="main":
-    
