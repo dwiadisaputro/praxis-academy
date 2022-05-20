be9071aebff07566bbe87b4883c68eccb129e2dc
dwiadisaputro <asmbrt@gmail.com>
2022-05-20 13:50:31 +0700

    buat database

--- crud/main.py
+++ crud/main.py
@@ -6,6 +6,13 @@ import pprint
 load_dotenv(find_dotenv())
 
 password = os.environ.get("MONGODB_PWD")
-print(password)
+# print(password)
 connection_string = f"mongodb+srv://dwi:{password}@cluster0.ylbba.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
+
+mydb = client["mydb"]
+mycol = mydb["riwayat"]
+
+datalist = [{'nama':'samosir', 'age':27, 'alamat':'sulawesi'}, {'nama':'zaki', 'age':20, 'alamat':'jawa'}, 
+            {'nama':'syafak', 'age':19, 'alamat':'rembang'}, {'nama':'ike', 'age':22, 'alamat':'pati'}]
+mycol.insert_many(datalist)
--- demo.py
+++ demo.py
@@ -1,4 +1,20 @@
+import collections
 import pymongo
-import dns
+import pprint
 
-client = pymongo.MongoClient("")
+# conn_str = "mongodb+srv://dwi:blkk57@cluster0.ylbba.mongodb.net/test"
+# client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+
+db = pymongo.MongoClient("mongodb://localhost:27017")
+
+#database
+dbmy = db["koleksi"]
+
+#collection
+collectionmy = dbmy["rak"]
+
+#data yg akan diinsert
+data = {"rak 1" : "kamus", "judul" : "kamus bhs inggris"}
+
+#data insert
+collectionmy.insert_one(data)
