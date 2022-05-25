27cac803f67cc0a21a6d8255d2e9682b27afba8a
dwiadisaputro <asmbrt@gmail.com>
2022-05-25 14:06:37 +0700

    flask-variabel

---
+++ enterprise-python/minggu03/hari02/flask/building/buil.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+    return 'Selamat Datang'
+
+@app.route('/anggota/<anggota>')
+def hello_anggota(anggota):
+    return f'Selamat datang {anggota}'
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name =='admin':
+        return redirect(url_for('hello_adm'))
+    else:
+        return redirect(url_for('hello_anggota', anggota = name))
+    
+
+if __name__ == '__main__':
+    app.run(debug = True)
